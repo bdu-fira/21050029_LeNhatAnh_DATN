0c8f5160ea757eb4d73f3afda679040ef87202f2
21050029-nhatanh <21050029@student.bdu.edu.vn>
2025-09-04 08:42:06 +0700

    Add files via upload

---
+++ SmartParking/app/build/generated/source/buildConfig/androidTest/debug/com/example/parkingmobiapp/test/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.parkingmobiapp.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.parkingmobiapp.debug.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final String API_BASE_URL = "https://dev-parking-system.com/api/";
+  // Field from build type: debug
+  public static final boolean DEBUG_MODE = true;
+  // Field from default config.
+  public static final String WEBSOCKET_URL = "wss://your-parking-system.com/mobile-ws";
+}
---
+++ SmartParking/app/build/generated/source/buildConfig/debug/com/example/parkingmobiapp/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.parkingmobiapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.parkingmobiapp.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0-debug";
+  // Field from build type: debug
+  public static final String API_BASE_URL = "https://dev-parking-system.com/api/";
+  // Field from build type: debug
+  public static final boolean DEBUG_MODE = true;
+  // Field from default config.
+  public static final String WEBSOCKET_URL = "wss://your-parking-system.com/mobile-ws";
+}
